d324dc0857203ab7dc19c5d247e2645b4ee0e0c0
Byron Lathi <bslathi19@gmail.com>
2019-12-29 13:43:52 -0500

    Change character size to 8x16, screen size to 80x30

--- imageconvert.py
+++ imageconvert.py
@@ -7,30 +7,18 @@ out = open("charset.bin", "w")
 bytelist = []
 
 '''
-So basically what it should be is each of the top half bytes followed by
-each of the bottom half bytes
+so it's 16 bytes, the top row of 8 down the the bottom row, then the next char
 '''
 
 for row in range(0, 10):
      for col in range(0, 10):
-        #print("===========")
-        #print (row)
-        #print (col)
-        #print("===========")
-        for subcol in range (0, 9):
-            byte = 0
-            for subrow in range (0, 8):
-                pixel = (image[row*8+subrow,col*9+subcol,0])/255
-                byte = byte + (pixel << subrow)
-            #print (format(byte, 'b') + ", " + format(byte, 'x'))
-            bytelist.append(byte)
-        for subcol in range (0, 9):
-            byte = 0
-            for subrow in range (8, 16):
-                pixel = (image[row*8+subrow,col*9+subcol,0])/255
-                byte = byte + (pixel << (subrow-8))
-            #print (format(byte, 'b') + ", " + format(byte,  'x'))
-            bytelist.append(byte)
+          for subrow in range (16):
+               byte = 0
+               for position in range (8):
+                    pixel = (image[row*16+subrow,col*8+position,0])/255
+                    byte = byte + (pixel << position)
+               #print (format(byte, 'b').zfill(8) + ", " + format(byte,  'x'))
+               bytelist.append(byte)
 
 outbytes = bytearray(bytelist)
 out.write(outbytes)
