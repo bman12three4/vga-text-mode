a678bcb874d423861b7455d3458a198ad6156a54
Byron Lathi <bslathi19@gmail.com>
2019-12-31 21:23:02 -0500

    Remove all user controlled stuff, right now you just have to directly change the memory inside

--- text_mode.v
+++ text_mode.v
@@ -1,41 +1,24 @@
 module text_mode (
 		input clk,
-
-		input [7:0] data_sw_i,
-		
-		input wren_i,
-		
-		input addr_inc_dir_i,
-		input addr_inc_i,
 		
 		output reg [3:0] r_vga_o,
 		output reg [3:0] g_vga_o,
 		output reg [3:0] b_vga_o,
 		
 		output reg v_sync_o,
-		output reg h_sync_o,
+		output reg h_sync_o
 		
-		output [6:0] addr_hex0_o,
-		output [6:0] addr_hex1_o,
-		output [6:0] addr_hex2_o,
-		output [6:0] data_hex_o,
-		
-		output [7:0] data_led_o,
-		output dir_led_o,
-		
-		output wren_led_o
 	);
 	
-	wire vga_clk;
+	(* keep *) wire vga_clk;
 	
-	reg [11:0] screen_address;
-	reg [11:0] user_address;
+	wire [11:0] screen_address /*synthesis keep */;
 
 	
 	reg wren_m;
 	
-	reg [6:0] screenX; //These are the 80x25 subacters
-	reg [4:0] screenY;
+	reg [6:0] screenX/*synthesis noprune */; //These are the 80x25 subacters
+	reg [4:0] screenY/*synthesis noprune */;
 	
 	reg [2:0] subX;	 //These are the 9x16 pixel subacters
 	reg [3:0] subY;
@@ -49,30 +32,10 @@ module text_mode (
 	
 	(* keep *) wire pixel;
 	
-	assign data_led_o = user_char;
+	assign screen_address [6:0] = screenX;
+	assign screen_address [11:7] = screenY;
+
 	
-	assign wren_led_o = wren_m;
-	assign dir_led_o = addr_inc_dir_i;
-	
-	seven_seg addr0 (
-		.in (user_address[3:0]),
-		.out (addr_hex0_o)
-	);
-	
-	seven_seg addr1 (
-		.in (user_address[7:4]),
-		.out (addr_hex1_o)
-	);
-	
-	seven_seg addr2 (
-		.in (user_address[11:8]),
-		.out (addr_hex2_o)
-	);
-	
-	seven_seg data (
-		.in (user_char[6:0]),
-		.out (data_hex_o)
-	);
 
 	vga_clk a (
 		.inclk0 (clk),
@@ -82,7 +45,7 @@ module text_mode (
 	screen_ram b (
 		.address (screen_address),
 		.clock (clk),
-		.data (data_sw_i),
+		.data (user_char),
 		.wren (wren_m),
 		.q (chr_val)
 	);
@@ -94,28 +57,6 @@ module text_mode (
 		.row (subY),
 		.pixel (pixel)
 	);
-	
-	always @(posedge addr_inc_i) begin
-		if (addr_inc_dir_i == 1) begin
-			user_address <= user_address + 11'b1;
-		end
-		else begin
-			user_address <= user_address - 11'b1;
-		end
-	end
-	
-	always @(posedge clk) begin
-		if (h_pixel < 640) begin
-			wren_m <= 0;
-			screen_address [6:0] <= screenX;
-			screen_address [11:7] <= screenY;
-		end
-		else begin
-			screen_address <= user_address;
-			user_char <= chr_val;
-			wren_m <= ~wren_i;
-		end
-	end
 	
 	always @(posedge vga_clk) begin
 
@@ -196,18 +137,3 @@ module text_mode (
 	
 
 endmodule //text_mode
-
-module seven_seg (
-		input [3:0] in,
-		output [6:0] out
-	);
-	
-	assign out [0] = (in == 4'h0 || in == 4'h2 || in == 4'h3 || (in >= 4'h5 && in <= 4'hA) || in == 4'hC || in == 4'hE || in == 4'hF) ? 1'b0 : 1'b1;
-	assign out [1] = (in <= 4'h4 || (in >= 4'h7 && in <= 4'hA) || in == 4'hD) ? 1'b0 : 1'b1;
-	assign out [2] = (in <= 4'h1 || (in >= 4'h3 && in <= 4'hB) || in  == 4'hD) ? 1'b0 : 1'b1;
-	assign out [3] = (in == 4'h0 || in == 4'h2 || in == 4'h3 || in == 4'h5 || in == 4'h6 || in == 4'h8 || in == 4'h9 || (in >= 4'hB && in <= 4'hE)) ? 1'b0 : 1'b1;
-	assign out [4] = (in == 4'h0 || in == 4'h2 || in == 4'h6 || in == 4'h8 || (in >= 4'hA && in <= 4'hF)) ? 1'b0 : 1'b1;
-	assign out [5] = (in == 4'h0 || (in >= 4'h4 && in <= 4'h6) || (in >= 4'h8 && in <= 4'hC) || in >= 4'hE) ? 1'b0 : 1'b1;
-	assign out [6] = ((in >= 4'h2 && in <= 4'h6) || (in >= 4'h8 && in <= 4'hB) || in >= 4'hD) ? 1'b0 : 1'b1;
-	
-endmodule //seven_seg
