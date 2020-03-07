2bbea7c48386ad65ae68d27e0fb12248dad1f695
Byron Lathi <byron.lathi@student.lovett.org>
2020-03-07 13:06:17 -0600

    I didn't do anything but somehow the timing issue was fixed (??)

--- chr_rom_ctrl.v
+++ chr_rom_ctrl.v
@@ -24,7 +24,7 @@ module chr_rom_ctrl (
 	
 	(* keep *) wire [7:0] pixel_mask;
 	
-	mux_3to8 b (
+	mux_8to1 b (
 		.in (col),
 		.out (pixel_mask)
 	);
@@ -33,8 +33,8 @@ module chr_rom_ctrl (
 																	// col would be 4, 00010000 >> 4 is 1, so on.
 endmodule //chr_rom_ctrl
 
-module mux_3to8 (
-	input [3:0] in,
+module mux_8to1 (
+	input [2:0] in,
 	output reg [7:0] out
 	);
 	
@@ -49,8 +49,8 @@ module mux_3to8 (
 			3'b110 : out = 8'b01000000;
 			3'b111 : out = 8'b10000000;
 			//To make sure that latches are not created create a default value for output.
-			default : out = 8'b00000000; 
+			default : out = 8'b00000000;
 		endcase
 
 	
-endmodule //mux_3to8
+endmodule //mux_8to1
